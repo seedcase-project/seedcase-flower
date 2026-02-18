73a5be2c690bbcde4f235601265c377ca368d123
martonvago <57952344+martonvago@users.noreply.github.com>
2026-02-18 13:21:31 +0000

    feat: :sparkles: add `Config` (#116)
    # Description
    
    This PR adds the `Config` class.
    Implemented with [this
    comment](https://github.com/seedcase-project/seedcase-flower/pull/112#discussion_r2808879904)
    in mind.
    
    Closes #25 
    
    Needs a thorough review.
    
    ## Checklist
    
    - [x] Formatted Markdown
    - [x] Ran `just run-all`
    
    ---------
    
    Co-authored-by: Luke W. Johnston <lwjohnst86@users.noreply.github.com>

--- src/seedcase_flower/__init__.py
+++ src/seedcase_flower/__init__.py
@@ -1,5 +1,7 @@
 """Module containing all source code."""
 
 from .cli import build, view
+from .config import Config
+from .internals import BuildStyle
 
-__all__ = ["build", "view"]
+__all__ = ["build", "view", "Config", "BuildStyle"]
---
+++ src/seedcase_flower/config.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+from typing import Optional
+
+from pydantic import BaseModel
+
+from seedcase_flower.internals import BuildStyle
+
+
+class Config(BaseModel, frozen=True):
+    """Configuration settings for styling the metadata.
+
+    See the
+    [design](https://flower.seedcase-project.org/docs/design/interface/config#config)
+    for an explanation of how Config is designed. See the
+    [guide](https://flower.seedcase-project.org/docs/guide/custom-styles) on how to
+    set up custom styles and sections.
+    See `Section` and `Content` help for more details on how to set up the sections.
+
+    Attributes:
+        style (BuildStyle): The built-in style to use for outputting the
+            documentation. Ignored when `template_dir` is set.
+        template_dir (Optional[Path]): When using a custom style, this should be the
+            relative directory path to the
+            [Jinja2](https://jinja.palletsprojects.com/en/stable/) template files.
+            The directory **must** contain at least one template Jinja2 file and
+            a `sections.toml` file that contains attributes for the `Section` classes.
+        output_dir (Path): The directory where output files will be saved.
+
+    Examples:
+        ```{python}
+        import seedcase_flower as fl
+        from pathlib import Path
+
+        # A config using the built-in `quarto-resource-listing` style and outputting
+        # to the `my-docs/` folder.
+        config = fl.Config(
+            style=fl.BuildStyle.quarto_resource_listing, output_dir=Path("my-docs/")
+        )
+
+        # A custom style that points to a template folder and outputs
+        # to the default `docs/` folder.
+        config = fl.Config(template_dir=Path("templates/"))
+
+        # A custom style that points to a template folder and outputs
+        # to the `my-docs/` folder.
+        config = fl.Config(template_dir=Path("templates/"), output_dir=Path("my-docs/"))
+        ```
+    """
+
+    style: BuildStyle = BuildStyle.quarto_one_page
+    template_dir: Optional[Path] = None
+    output_dir: Path = Path("docs")
