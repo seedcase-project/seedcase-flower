40054553f555a7ad21cdb294a7312ba02bba07de
Joel Ostblom <joel.ostblom@gmail.com>
2026-02-27 15:16:06 +0100

    refactor: ♻️ Name variables more precisely
    I think it was a bit confusing
    that we were calling the input string a uri
    although it could also be a file or folder path.
    We ended up having functions named `_convert_to_*uri()`
    that already took a `uri` variable as the input argument
    which semantically doesn't make much sense.

--- src/seedcase_flower/internals.py
+++ src/seedcase_flower/internals.py
@@ -32,13 +32,13 @@ def _parse_source(source: str) -> Uri:
         case "file":
             return _convert_to_file_uri(split_source)
         case "https":
-            return _convert_to_https_uri(split_uri)
+            return _convert_to_https_uri(split_source)
         case "gh" | "github":
-            return _convert_to_github_uri(split_uri)
+            return _convert_to_github_uri(split_source)
         case _:
             raise ValueError(
-                "The URI must be either a path to an existing file/folder "
-                "or have one of the following URI prefixes: "
+                "The source must be either a path to an existing file/folder "
+                "or a URI with one of the following URI prefixes: "
                 "`file:`, `https:`, `gh:`, `github:`"
             )
 
@@ -51,20 +51,18 @@ def _convert_to_file_uri(split_file_source: parse.SplitResult) -> Uri:
     return Uri(value=split_file_source.geturl(), local=True)
 
 
-def _convert_to_file_uri(split_uri: parse.SplitResult) -> Uri:
-    return _convert_to_path(parse.unquote(split_uri.path))
+def _convert_to_https_uri(split_https_source: parse.SplitResult) -> Uri:
+    return Uri(value=split_https_source.geturl(), local=False)
 
 
-def _convert_to_https_uri(split_uri: parse.SplitResult) -> Uri:
-    return Uri(value=split_uri.geturl(), local=False)
-
-
-def _convert_to_github_uri(split_uri: parse.SplitResult) -> Uri:
+def _convert_to_github_uri(split_gh_source: parse.SplitResult) -> Uri:
     github_path = "/".join(
-        part for part in (split_uri.netloc, split_uri.path.lstrip("/")) if part
+        part
+        for part in (split_gh_source.netloc, split_gh_source.path.lstrip("/"))
+        if part
     )
     return Uri(
-        value=split_uri._replace(
+        value=split_gh_source._replace(
             scheme="https",
             netloc="raw.githubusercontent.com",
             path=f"/{github_path}/refs/heads/main/datapackage.json",
