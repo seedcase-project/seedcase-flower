ba52414ab2e1f8b36995507d5b575fecdeb48bb8
Joel Ostblom <joel.ostblom@gmail.com>
2026-03-11 18:06:24 +0100

    refactor: ♻️ Simplify by importing more from check_datapackage

--- src/seedcase_flower/errors.py
+++ src/seedcase_flower/errors.py
@@ -1,34 +1,8 @@
 """Custom exception handling for seedcase-flower."""
 
-import sys
 from urllib.error import HTTPError, URLError
 
-from check_datapackage import (
-    DataPackageError,
-    create_no_traceback_hook,
-    create_no_traceback_ipython_handler,
-)
+from check_datapackage import setup_no_traceback_hooks
 
 
-sys.excepthook = create_no_traceback_hook(
-    DataPackageError, FileNotFoundError, HTTPError, URLError
-)
-
-
-def _is_running_from_ipython() -> bool:
-    """Checks whether running in IPython interactive console or not."""
-    try:
-        from IPython import get_ipython
-    except ImportError:
-        return False
-    else:
-        return get_ipython() is not None
-
-
-if _is_running_from_ipython():
-    get_ipython().set_custom_exc(  # type: ignore[misc]
-        (Exception,),
-        create_no_traceback_ipython_handler(
-            DataPackageError, FileNotFoundError, HTTPError, URLError
-        ),
-    )
+setup_no_traceback_hooks(FileNotFoundError, HTTPError, URLError)
